f4a58f801d1ee903cb4496aebef73f43a58fd336
Ernesto Diaz <diaz37@usf.edu>
2025-10-19 23:23:30 -0400

    created fetchProductsAsync()

--- cp_2.js
+++ cp_2.js
@@ -1,10 +1,29 @@
+// define function to retrieve data using .then() method
 function fetchProductsThen() {
     fetch('https://www.course-api.com/javascript-store-products')
         .then((response) => {
             console.log(response);
+            return response.json();
         })
-        .catch((error) => {
-            console.log(error);
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(err);
         });
 };
 
+// define function to retrieve data using async/await
+async function fetchProductsAsync() {
+    try {
+        const response = await fetch('https://www.course-api.com/javascript-store-products');
+
+        const products = await response.json();
+    
+        displayProducts(products);
+    } catch (err) {
+        handleError(err);
+    }
+}
+
+fetchProductsAsync()
